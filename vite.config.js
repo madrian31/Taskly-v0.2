387202b56bd939411657ed9e083c04c09fed2da1
BernalesMarc <102293703+BernalesMarc@users.noreply.github.com>
2026-01-06 01:21:16 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Taskly-v0.2/',
-    server: {
-      port:5500,
-      host: true,
-    }
-
-
+  build: {
+    outDir: 'docs'
+  }
 })
