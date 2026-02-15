750d8b79f2e187f6d865c037665ec9e2625574ff
mBernales659 <marcadriancbernales699@gmail.com>
2026-02-15 14:55:30 +0800

    update

--- src/components/ui/task/emojiRatingPicker/Emojiratingpicker.tsx
+++ src/components/ui/task/emojiRatingPicker/Emojiratingpicker.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Emojiratingpicker.css';
+// import './EmojiRatingPicker.css';    
 import {
   DifficultyEmoji,
   CompletionMoodEmoji,
--- src/components/ui/task/modal/TaskModal.tsx
+++ src/components/ui/task/modal/TaskModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './TaskModal.css';
+import './../emojiRatingPicker/EmojiRatingPicker.css';
 import {
     FileUp,
     File,
